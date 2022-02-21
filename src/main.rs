ab75bce09aed508b585b12b7074903575ddcddbc
Scott Haiden <scott.b.haiden@gmail.com>
2022-02-20 19:20:47 -0800

    Prime sieve exists

---
+++ src/main.rs
@@ -0,0 +1,39 @@
+use std::io::Write;
+
+fn is_prime(i: u32, primes: &Vec<u32>) -> bool {
+    let limit = (i as f64).sqrt() as u32;
+    for prime in primes {
+        if prime > &limit { break; }
+        if i % prime == 0 { return false; }
+    }
+    return true;
+}
+
+fn print_progress(i: u32) {
+    let mask = 0xffff;
+    let masked = i & mask;
+    if masked != mask { return; }
+    let factor = 100f64 / (u32::MAX as f64);
+    let percent = factor * i as f64;
+    eprintln!("{:08x} ({:02.5})", i, percent);
+}
+
+fn main() {
+    let mut stdout = std::io::stdout();
+
+    let max = u32::MAX;
+    let max_track = (max as f64).sqrt() as u32;
+
+    let mut primes = Vec::new();
+
+    for i in (3..max).step_by(2) {
+        print_progress(i);
+        if !is_prime(i, &primes) { continue; }
+
+        let bytes = i.to_be_bytes();
+        stdout.write_all(&bytes).expect("write failed");
+
+        if i > max_track { continue; }
+        primes.push(i);
+    };
+}

c7ef3d22ba07db8cd63674555752961d61aa965f
Scott Haiden <scott.b.haiden@gmail.com>
2022-02-20 21:05:43 -0800

    Change the output.

--- src/main.rs
+++ src/main.rs
@@ -9,13 +9,13 @@ fn is_prime(i: u32, primes: &Vec<u32>) -> bool {
     return true;
 }
 
-fn print_progress(i: u32) {
+fn print_progress(i: u32, found: u32) {
     let mask = 0xffff;
     let masked = i & mask;
     if masked != mask { return; }
     let factor = 100f64 / (u32::MAX as f64);
     let percent = factor * i as f64;
-    eprintln!("{:08x} ({:02.5})", i, percent);
+    eprintln!("{:08x} ({:02.5}%) {:10}", i, percent, found);
 }
 
 fn main() {
@@ -24,12 +24,14 @@ fn main() {
     let max = u32::MAX;
     let max_track = (max as f64).sqrt() as u32;
 
+    let mut found = 0u32;
     let mut primes = Vec::new();
 
     for i in (3..max).step_by(2) {
-        print_progress(i);
+        print_progress(i, found);
         if !is_prime(i, &primes) { continue; }
 
+        found += 1;
         let bytes = i.to_be_bytes();
         stdout.write_all(&bytes).expect("write failed");
 
